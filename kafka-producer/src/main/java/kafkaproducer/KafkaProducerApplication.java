8f3369d4f144ecde24f5bf930a29eeba44ab891e
andrey1510 <andrey-1510@outlook.com>
2023-06-22 21:08:55 +0300

    Initial commit

---
+++ kafka-producer/src/main/java/kafkaproducer/KafkaProducerApplication.java
@@ -0,0 +1,57 @@
+package kafkaproducer;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class KafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProducerApplication.class, args);
+    }
+
+    @Bean
+    public KafkaAdmin admin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name("topic1")
+                .partitions(10)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public KafkaAdmin.NewTopics topics456() {
+        return new KafkaAdmin.NewTopics(
+                TopicBuilder.name("defaultBoth")
+                        .build(),
+                TopicBuilder.name("defaultPart")
+                        .replicas(1)
+                        .build(),
+                TopicBuilder.name("defaultRepl")
+                        .partitions(3)
+                        .build());
+    }
+
+    @Bean
+    public ApplicationRunner runner(KafkaTemplate<String, String> template) {
+        return args -> template.send("topic1", "test");
+    }
+
+}
